4b6a5d6452509d9e741ecd931b9ab6d55eb95337
Micaela Alves <micaela.alves93@gmail.com>
2022-02-14 04:46:35 -0300

    feat - algoritimo de busca linear

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+    for std in permanence_period:
+        if std[0] is None or std[1] is None:
+            return None
+        if std[0] <= target_time <= std[1]:
+            counter += 1
+    return counter
